73c4694759ce1bba97b6f675cc1469f0af114ec1
Thibault Poisson <thibault.poisson@gmail.com>
2011-05-27 14:24:24 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EtudeLegendre::Application.config.secret_token = 'b84b9d9dfabfb0e49c03a9d983dcef786eaa47f75177b269eba71f4cfb3af5a3caff86509e87fb24aa9641591bdd78480c84c86931396f161e21842eea613239'
