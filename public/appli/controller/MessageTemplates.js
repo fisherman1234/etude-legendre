61de9e9c9a4b3e0186c5a109b653eb24c2766aa8
Thibault Poisson <thibault.poisson@gmail.com>
2011-11-22 00:28:18 +0100

    evolution generateur messages

--- public/appli/controller/MessageTemplates.js
+++ public/appli/controller/MessageTemplates.js
@@ -11,36 +11,38 @@ Ext.define('TP.controller.MessageTemplates', {
             }
         });
     },
-    save: function(button){
-      var win = button.up("window");
-      contactForm = win.down('form');
-			if (contactForm.form.isValid()){
-			  record = contactForm.getRecord();
-        values = contactForm.getValues();
-				if (typeof record == "undefined"){
-					record = Ext.ModelManager.create({},
-	        'TP.model.MessageTemplate');
-					Ext.getStore('TP.store.MessageTemplates').insert(0, record);
-				}
-        record.set(values);
-        Ext.getStore('TP.store.MessageTemplates').sync();
-        
-        var timer2 = setInterval(function() {
-            
-            if (!record.phantom) {
-                clearInterval(timer2);
-                var rec  = Ext.getStore('TP.store.TypeActivites').getAt(win.parentRowIndex);
+    save: function(button) {
+        var win = button.up("window");
+        contactForm = win.down('form');
+        if (contactForm.form.isValid()) {
+            record = contactForm.getRecord();
+            values = contactForm.getValues();
+            if (typeof record == "undefined") {
+                record = Ext.ModelManager.create({},
+                'TP.model.MessageTemplate');
+                Ext.getStore('TP.store.MessageTemplates').insert(0, record);
+            }
+            record.set(values);
+            Ext.getStore('TP.store.MessageTemplates').sync();
+            if (record.phantom) {
+                var timer2 = setInterval(function() {
+                    var record = Ext.getStore('TP.store.MessageTemplates').getAt(0);
+                    if (!record.phantom) {
+                        clearInterval(timer2);
+                        var rec = Ext.getStore('TP.store.TypeActivites').getAt(win.parentRowIndex);
+                        rec.set('message_template_id', record.data.id);
+                        rec.store.sync();
+                        win.close();
+                    }
+                },
+                200);
+            } else {
+                var rec = Ext.getStore('TP.store.TypeActivites').getAt(win.parentRowIndex);
                 rec.set('message_template_id', record.data.id);
                 rec.store.sync();
                 win.close();
-                
-
             }
-        },
-        200);
-			}
-      
+        }
     }
 
-   
 });
