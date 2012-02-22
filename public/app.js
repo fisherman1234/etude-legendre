1ee257feae53c5963b50cf11b8d2e6dfd3d4e987
Thibault Poisson <thibault.poisson@gmail.com>
2012-02-22 13:01:18 +0100

    ajout d'un fix pour l'ipad

--- public/app.js
+++ public/app.js
@@ -69,12 +69,70 @@ Ext.override(Ext.form.HtmlEditor, {
     cleanDefaultValue: true,
     cleanHtml: function(html) {
         html = String(html);
-        if(Ext.isWebKit){
+        if (Ext.isWebKit) {
             html = html.replace(/\sclass="(?:Apple-style-span|khtml-block-placeholder)"/gi, '');
         }
-        if(this.cleanDefaultValue){
+        if (this.cleanDefaultValue) {
             html = html.replace(new RegExp(this.defaultValue), '');
         }
         return html;
     }
+});
+// pour le scroll sur ipad
+Ext.override(Ext.Panel, {
+    afterRender: Ext.Panel.prototype.afterRender.createSequence(function() {
+        if (this.getXType() == 'panel') {
+            this._getIScrollElement = function() {
+                return (this.el.child('.x-panel-body', true));
+            };
+        }
+
+        //Uncomment below to use iScroll only on mobile devices but use regular scrolling on PCs.
+        if (this.autoScroll && Ext.isMobileDevice) {
+            if (this._getIScrollElement) {
+                this._updateIScroll();
+                this.on('afterlayout', this._updateIScroll);
+            }
+        }
+    }),
+
+    _ensureIScroll: function() {
+        if (!this.iScroll) {
+            var el = this._getIScrollElement();
+            if (el.children.length > 0) {
+                this.iScroll = new iScroll(el);
+                this.iScrollTask = new Ext.util.DelayedTask(this._refreshIScroll, this);
+            }
+        }
+    },
+
+    _updateIScroll: function() {
+        this._ensureIScroll();
+        if (this.iScroll) {
+            this.iScrollTask.delay(1000);
+        }
+    },
+
+    _refreshIScroll: function() {
+        this.iScroll.refresh();
+        //Refresh one more time.
+        this.iScrollTask.delay(1000);
+    }
+});
+
+Ext.override(Ext.tree.TreePanel, {
+    _getIScrollElement: function() {
+        return (this.el.child('.x-panel-body', true));
+    }
+});
+
+Ext.override(Ext.grid.GridPanel, {
+    _getIScrollElement: function() {
+        return (this.el.child('.x-grid3-scroller', true));
+    },
+
+    afterRender: Ext.grid.GridPanel.prototype.afterRender.createSequence(function() {
+        //TODO: need to hook into more events and to update iScroll.
+        this.view.on('refresh', this._updateIScroll, this);
+    })
 });
